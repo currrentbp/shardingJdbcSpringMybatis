bb7eb518bd78c58ad56a43cec59e555537e459fd
baopan <baopan@gaosiedu.com>
2017-11-07 11:40:08 +0800

    new project for shardingjdbc

---
+++ src/test/java/test/Test1.java
@@ -0,0 +1,16 @@
+package test;
+
+import base.BaseTest;
+import org.junit.Test;
+
+/**
+ * @author current_bp
+ * @createTime 20171107
+ */
+public class Test1 extends BaseTest {
+
+    @Test
+    public void t1(){
+        System.out.println("======");
+    }
+}
